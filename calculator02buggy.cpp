d881941f2a5df49988aee7d99aab59299f157db1
jooszebi26 <43747192+jooszebi26@users.noreply.github.com>
2021-11-10 23:08:37 +0100

    Update calculator02buggy.cpp

--- calculator02buggy.cpp
+++ calculator02buggy.cpp
@@ -49,7 +49,6 @@ private:
 Token_stream::Token_stream()
 :full(false), buffer(0)    // no Token in buffer
 {
-    
 }
 
 //------------------------------------------------------------------------------
@@ -64,7 +63,7 @@ void Token_stream::putback(Token t)
 
 //------------------------------------------------------------------------------
 
-Token get()
+Token Token_stream::get()
 {
     if (full) {       // do we already have a Token ready?
         // remove token from buffer
@@ -77,15 +76,15 @@ Token get()
 
     switch (ch) {
     case '=':    // for "print"
-    case 'x':    // for "quit"
+    case 'q':    // for "quit"
     case '(': case ')': case '+': case '-': case '*': case '/': 
         return Token(ch);        // let each character represent itself
     case '.':
     case '0': case '1': case '2': case '3': case '4':
-    case '5': case '6': case '7': case '9':
+    case '5': case '6': case '7': case '8': case '9':
         {    
             cin.putback(ch);         // put digit back into the input stream
-            double val = 0;
+            double val;
             cin >> val;              // read a floating-point number
             return Token('8',val);   // let '8' represent "a number"
         }
@@ -113,7 +112,7 @@ double primary()
         {    
             double d = expression();
             t = ts.get();
-            if (t.kind != ')') error("')' expected'");
+            if (t.kind != ')') error("')' expected");
             return d;
         }
     case '8':            // we use '8' to represent a number
@@ -179,15 +178,18 @@ double expression()
 //------------------------------------------------------------------------------
 
 int main(){
-    cout << "Welcome to our simple calculator.
-Please enter expressions using floating-point numbers."<< endl;
-    cout << "Avaible operators: +,-,*,/,%" << endl;
 try
 {
+    cout<< "Welcome to our simple calculator."<<endl;
+    cout<<"Please enter expressions using floating-point numbers."<<endl;
+    cout<<"Avaible operators: +,-,*,/,(,)"<<endl;
+    cout<<"Result:="<<endl;
+    cout <<"Quit:q"<<endl;
+    double val = 0.00;
     while (cin) {
         Token t = ts.get();
 
-        if (t.kind == 'x') break; // 'q' for quit
+        if (t.kind == 'q') break; // 'q' for quit
         if (t.kind == '=')        // ';' for "print now"
             cout << "=" << val << '\n';
         else
